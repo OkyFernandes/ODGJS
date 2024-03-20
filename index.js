10a2ca7490bb4211c38f5ad1b70b288015f49cc0
trilida <164031460+trilida@users.noreply.github.com>
2024-03-20 21:02:03 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,36 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, Menu } = require("electron"); // Jangan lupa untuk impor Menu
 
 function createWindow() {
   const mainWindow = new BrowserWindow({
-    width: 800, // Perbaiki dari "heigth" ke "height" dan tambahkan "width"
+    width: 800,
     height: 600,
     webPreferences: {
       nodeIntegration: true,
-      contextIsolation: false, // Tambahkan ini untuk menggunakan nodeIntegration dengan aman
+      contextIsolation: false,
     },
   });
 
+  // Menghilangkan menu bar
+  Menu.setApplicationMenu(null);
+
+  // Memuat file HTML utama aplikasi
   mainWindow.loadFile("index.html");
-  mainWindow.webContents.openDevTools(); // Opsional: Buka DevTools untuk debugging
+
+  // Buka DevTools 
+  mainWindow.webContents.openDevTools();
 }
 
-app.whenReady().then(() => {
-  createWindow();
+app.whenReady().then(createWindow);
 
-  app.on("activate", () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow();
-    }
-  });
+app.on("activate", () => {
+  // Pada macOS umumnya biasa untuk membuat jendela baru ketika ikon diklik dan tidak ada jendela lain yang terbuka.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
 });
 
 app.on("window-all-closed", () => {
+  // Pada macOS aplikasi dan menu bar umumnya tetap aktif sampai pengguna keluar secara eksplisit dengan Cmd + Q
   if (process.platform !== "darwin") {
     app.quit();
   }
